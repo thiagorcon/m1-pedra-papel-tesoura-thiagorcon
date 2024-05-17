316e159e94071d8a1cee937f6b675e99e4deb9f1
Thiago Rosa <thiagorcon@hotmail.com>
2024-05-17 10:52:26 -0300

    reenvio da função usando operador II

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 function playRockPaperScissor(jogador1, jogador2) {
-  let vencedor = "";
-  if (jogador1 == "Pedra" && jogador2 == "Pedra") {
-    return (vencedor = "Empate!");
-  } else if (jogador1 == "Papel" && jogador2 == "Papel") {
-    return (vencedor = "Empate!");
-  } else if (jogador1 == "Tesoura" && jogador2 == "Tesoura") {
-    return (vencedor = "Empate!");
-  } else if (jogador1 == "Papel" && jogador2 == "Tesoura") {
-    return (vencedor = "Jogador 2 venceu!");
-  } else if (jogador1 == "Tesoura" && jogador2 == "Pedra") {
-    return (vencedor = "Jogador 2 venceu!");
-  } else if (jogador1 == "Pedra" && jogador2 == "Papel") {
-    return (vencedor = "Jogador 2 venceu!");
-  } else if (jogador2 == "Papel" && jogador1 == "Tesoura") {
-    return (vencedor = "Jogador 1 venceu!");
-  } else if (jogador2 == "Tesoura" && jogador1 == "Pedra") {
-    return (vencedor = "Jogador 1 venceu!");
-  } else if (jogador2 == "Pedra" && jogador1 == "Papel") {
-    return (vencedor = "Jogador 1 venceu!");
+  if (jogador1 === jogador2) {
+    return "Empate!";
+  } else if (
+    (jogador1 === "Papel" && jogador2 === "Tesoura") ||
+    (jogador1 === "Tesoura" && jogador2 === "Pedra") ||
+    (jogador1 === "Pedra" && jogador2 === "Papel")
+  ) {
+    return "Jogador 2 venceu!";
+  } else if (
+    (jogador2 === "Papel" && jogador1 === "Tesoura") ||
+    (jogador2 === "Tesoura" && jogador1 === "Pedra") ||
+    (jogador2 === "Pedra" && jogador1 === "Papel")
+  ) {
+    return "Jogador 1 venceu!";
   }
 }
+
 console.log(playRockPaperScissor());
